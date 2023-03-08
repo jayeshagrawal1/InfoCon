f19382f3f90088d866160797ae370a575f435fcc
jayeshagrawal1 <jayeshagra2002@gmail.com>
2023-03-08 17:13:10 +0530

    change in stuffs

--- script.js
+++ script.js
@@ -11,22 +11,73 @@ userSearch.addEventListener("input",(e)=>{
     })
 })
 
-let url="https://kontests.net/api/v1/all";
-let response=fetch(url)
-response.then(res=> res.json()
-).then(data=>{
-    users=data.map(user=>{
+fetchContestDetails();
+
+async function fetchContestDetails()
+{
+    try{
+        const response= await fetch("https://kontests.net/api/v1/all");
+    
+        const data=await response.json();
+
+        renderContestDetails(data);
+    }
+    catch(err)
+    {
+        console.log(err);
+        const errorHandlerDiv=document.querySelector(".ErrorHandling");
+        errorHandlerDiv.append("Cannot Fetch Details right now please try again later");
+    }
+}
+
+function renderContestDetails(data){
+    data.forEach(user=>{
+
         const card=userdataTemplate.content.cloneNode(true).children[0]
-        const title=card.querySelector(".card-title")
-        const txt1=card.querySelector(".card-text1")
-        const txt2=card.querySelector(".card-text2")
-        const txt4=card.querySelector(".card-text4")
-        const txt5=card.querySelector(".card-text5")
+        const title=card.querySelector(".card-title");
+        const txt1=card.querySelector(".card-text1");
+        const txt2=card.querySelector(".card-text2");
+        const txt4=card.querySelector(".card-text4");
+        const txt5=card.querySelector(".card-text5");
 
-        let img=card.querySelector("#card-img-top")
-        img.src=(user.url.includes("hackerearth"))?"he.png":(user.url.includes("leetcode"))?"lc.png":(user.url.includes("codeforces"))?"cf.jpg":(user.url.includes("codechef"))?"cc.jpg":(user.url.includes("hackerrank"))?"hr.png":(user.url.includes("google"))?"google.jpg":"ac.jpg";
-
-        let pf=(user.url.includes("hackerearth"))?"Hackerearth":(user.url.includes("leetcode"))?"Leetcode":(user.url.includes("codeforces"))?"Codeforces":(user.url.includes("codechef"))?"Codechef":(user.url.includes("hackerrank"))?"Hackerrank":(user.url.includes("hackerrank"))?"Google":"Atcoder";
+        let platform="";
+        let img=card.querySelector("#card-img-top");
+        if(user.url.includes("hackerearth"))
+        {
+            img.src="images/he.png";
+            platform='HackerEarth';
+        }
+        else if(user.url.includes("hackerrank"))
+        {
+            img.src="images/hr.png";
+            platform='HackerRank';
+        }
+        else if(user.url.includes("leetcode"))
+        {
+            img.src="images/lc.png";
+            platform='Leetcode';
+        }
+        else if(user.url.includes("codeforces"))
+        {
+            img.src="images/cf.jpg";
+            platform='CodeForces';
+        }
+        else if(user.url.includes("codechef"))
+        {
+            img.src="images/cc.jpg";
+            platform='CodeChef';
+        }
+        else if(user.url.includes("google"))
+        {
+            img.src="images/google.jpg";
+            platform='Google';
+        }
+        else
+        {
+            img.src="images/ac.jpg";
+            platform='AtCoder';
+        }
+        
         // start time formatting
         var date = user['start_time'].split('UTC')[0].split('T')[0];
         var time = user.start_time.split('.')[0].split('T')[1];
@@ -42,15 +93,14 @@ response.then(res=> res.json()
         else
         txt2.textContent+=date+"  "+time;
 
-        txt4.textContent+=pf;
+        txt4.textContent+=platform;
         txt5.textContent+=user.in_24_hours;
         title.textContent=user.name;
         let a=card.querySelector(".btn-primary")
         a.href=user.url
         userCardContainer.append(card)
-        return {name:user.name, platform: pf, element:card}
     })
-})
+}
 
 
 let search=document.getElementById('search');
@@ -71,6 +121,11 @@ search.addEventListener('mouseout',(e)=>{
 })
 
 search.addEventListener('touchend',(e)=>{
+    e.preventDefault()
+    search.style.backgroundColor='#314894';
+
+})
+search.addEventListener('keyup',(e)=>{
     e.preventDefault()
     search.style.backgroundColor='#314894';
 
